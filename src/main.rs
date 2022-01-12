37cfab20fecb9865cbf19d0fab6670d25d550395
Aarya Patel <aarya.patel@gmail.com>
2022-01-12 18:36:40 -0500

    More performance improvements

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,10 @@ use noise::{NoiseFn, Perlin};
 
 const WIDTH: usize = 640;
 const HEIGHT: usize = 360;
-const SPACE_SIZE: u32 = 50;
+const SPACE_SIZE: u32 = 6;
 
 fn make_coord(coord: [i32; 3]) -> usize {
-    let coord = coord.map(|i| (i + SPACE_SIZE as i32 / 2) as u32);
-    (coord[0] + coord[1] * SPACE_SIZE + coord[2] * SPACE_SIZE * SPACE_SIZE) as usize
+    coord.map(|i| (i + (1u32 << (SPACE_SIZE - 1)) as i32 - 1) as u32).iter().fold(0, |acc, i| (acc << SPACE_SIZE) + i) as usize
 }
 
 fn cast_ray(angle: (f32, f32), x: f32, y: f32, z: f32, space: &Vec<u32>) -> u32 {
@@ -21,7 +20,7 @@ fn cast_ray(angle: (f32, f32), x: f32, y: f32, z: f32, space: &Vec<u32>) -> u32 
         let mut min_index = 0;
         let total_dist: i32 = map.iter().fold(0, |acc, i| acc + i * i);
         
-        if total_dist > (SPACE_SIZE / 2 - 1).pow(2) as i32 {
+        if total_dist > (((1 << SPACE_SIZE - 1) - 1) as i32).pow(2) {
             break 0;
         }
 
@@ -44,16 +43,20 @@ fn cast_ray(angle: (f32, f32), x: f32, y: f32, z: f32, space: &Vec<u32>) -> u32 
             }
         }
 
-        side_dist[min_index] += delta_dist[min_index];
         map[min_index] += step[min_index];
 
-        if make_coord(map) >= space.len() {
+        let coord = make_coord(map);
+
+        if coord >= space.len() {
             break 0;
         }
-        let voxel = space[make_coord(map)];
+
+        let voxel = space[coord];
         if voxel != 0 {
             break voxel; 
         }
+
+        side_dist[min_index] += delta_dist[min_index];
     }
 }
 
@@ -72,15 +75,16 @@ fn gen_buffer(width: usize, height: usize, camera_angle: f32, space: &Vec<u32>) 
 
 fn gen_space() -> Vec<u32> {
     let perlin = Perlin::new();
-    let mut space: Vec<u32> = vec![0u32; SPACE_SIZE.pow(3) as usize];
-    for x in -(SPACE_SIZE as i32 / 2)..(SPACE_SIZE as i32 / 2) {
-        for y in -(SPACE_SIZE as i32 / 2)..(SPACE_SIZE as i32 / 2) {
+    let mut space: Vec<u32> = vec![0u32; 1usize << (SPACE_SIZE * 3)];
+    let half_space: i32 = (1i32 << (SPACE_SIZE - 1)) - 1;
+    for x in -half_space..half_space {
+        for y in -half_space..half_space {
             let height = (perlin.get([x as f64 / 10.0, y as f64 / 10.0]) *  10.0).floor() as i32; 
-            for z in -(SPACE_SIZE as i32 / 2)..height {
+            for z in -half_space..height {
                 let mut color = 0x00000000;
                 for i in [x, y, z] {
                     color = color << 8;
-                    color += (i + SPACE_SIZE as i32 / 2) as u32 * 2;
+                    color += (i + half_space) as u32 * 2;
                 }
                 space[make_coord([x, y, z])] = color;
             }
