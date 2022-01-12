c3f31930c9bd4e50bd206f6dad32325984541d25
Aarya Patel <aarya.patel@gmail.com>
2022-01-11 23:16:12 -0500

    Changed length of edge of space to constant SPACE_SIZE

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ use noise::{NoiseFn, Perlin};
 
 const WIDTH: usize = 640;
 const HEIGHT: usize = 360;
+const SPACE_SIZE: u32 = 50;
 
 fn make_coord(coord: [i32; 3]) -> usize {
-    let coord = coord.map(|i| (i + 50) as u32);
-    (coord[0] + coord[1] * 100 + coord[2] * 10_000) as usize
+    let coord = coord.map(|i| (i + SPACE_SIZE as i32 / 2) as u32);
+    (coord[0] + coord[1] * SPACE_SIZE + coord[2] * SPACE_SIZE * SPACE_SIZE) as usize
 }
 
 fn cast_ray(angle: (f32, f32), x: f32, y: f32, z: f32, space: &Vec<u32>) -> u32 {
@@ -18,14 +19,14 @@ fn cast_ray(angle: (f32, f32), x: f32, y: f32, z: f32, space: &Vec<u32>) -> u32 
 
     loop {
         let mut min_index = 0;
-        let mut total_dist = 0.0;
+        let mut total_dist: f32 = 0.0;
         for (k, v) in side_dist.iter().enumerate() {
             if v < &side_dist[min_index] {
                 min_index = k;
             }
             total_dist += (map[k] * map[k]) as f32;
         }
-        if total_dist > 2400.0 {
+        if total_dist > (SPACE_SIZE / 2 - 1).pow(2) as f32 {
             break 0;
         }
         side_dist[min_index] += delta_dist[min_index];
@@ -48,7 +49,7 @@ fn gen_buffer(width: usize, height: usize, camera_angle: f32, space: &Vec<u32>) 
             buffer[y * width + x] = cast_ray((
                     std::f32::consts::PI * (1.0 / 3.0 + (y as f32 / height as f32) / 2.0), 
                     camera_angle + std::f32::consts::PI * (1.0 / 4.0 + (x as f32 / width as f32) / 2.0)
-                    ), 0.0, 0.0, 25.0, space);
+                    ), 0.0, 0.0, 10.0, space);
         }
     }
     buffer
@@ -56,15 +57,15 @@ fn gen_buffer(width: usize, height: usize, camera_angle: f32, space: &Vec<u32>) 
 
 fn gen_space() -> Vec<u32> {
     let perlin = Perlin::new();
-    let mut space: Vec<u32> = vec![0u32; 1_000_000];
-    for x in -50..50i32 {
-        for y in -50..50i32 {
+    let mut space: Vec<u32> = vec![0u32; SPACE_SIZE.pow(3) as usize];
+    for x in -(SPACE_SIZE as i32 / 2)..(SPACE_SIZE as i32 / 2) {
+        for y in -(SPACE_SIZE as i32 / 2)..(SPACE_SIZE as i32 / 2) {
             let height = (perlin.get([x as f64 / 10.0, y as f64 / 10.0]) *  10.0).floor() as i32; 
-            for z in -50..height {
+            for z in -(SPACE_SIZE as i32 / 2)..height {
                 let mut color = 0x00000000;
                 for i in [x, y, z] {
                     color = color << 8;
-                    color += (i + 50) as u32 * 2;
+                    color += (i + SPACE_SIZE as i32 / 2) as u32 * 2;
                 }
                 space[make_coord([x, y, z])] = color;
             }
