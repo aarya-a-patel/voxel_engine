9610c620d463622c04ede172e7875e58cf7cb065
Aarya Patel <aarya.patel@gmail.com>
2022-01-11 22:46:44 -0500

    Changed to render within a pre-defined space

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,12 @@ use noise::{NoiseFn, Perlin};
 const WIDTH: usize = 640;
 const HEIGHT: usize = 360;
 
-fn get_voxel(x: f32, y: f32, z: f32, perlin: &Perlin) -> (bool, u32) {
-    if (perlin.get([x as f64 / 10.0, y as f64 / 10.0]) *  10.0).floor() >= z.floor() as f64 {
-        return (true, (z.abs() as u32) * 0x00040000 + (x.abs() as u32) * 0x00000400
-                + (y.abs() as u32) * 0x00000004);
-    }
-
-    (false, 0)
+fn make_coord(coord: [i32; 3]) -> usize {
+    let coord = coord.map(|i| (i + 50) as u32);
+    (coord[0] + coord[1] * 100 + coord[2] * 10_000) as usize
 }
 
-fn cast_ray(angle: (f32, f32), x: f32, y: f32, z: f32, perlin: &Perlin) -> u32 {
+fn cast_ray(angle: (f32, f32), x: f32, y: f32, z: f32, space: &Vec<u32>) -> u32 {
     let ray_dir: [f32; 3] = [angle.0.sin() * angle.1.cos(), angle.0.sin() * angle.1.sin(), angle.0.cos()];
     let delta_dist: [f32; 3] = ray_dir.map(|i| (1f32 / i).abs());
     let step: [i32; 3] = ray_dir.map(|i| (i / i.abs()) as i32);
@@ -29,32 +25,57 @@ fn cast_ray(angle: (f32, f32), x: f32, y: f32, z: f32, perlin: &Perlin) -> u32 {
             }
             total_dist += (map[k] * map[k]) as f32;
         }
-        if total_dist > 2500.0 {
+        if total_dist > 2400.0 {
             break 0;
         }
         side_dist[min_index] += delta_dist[min_index];
         map[min_index] += step[min_index];
-        let voxel = get_voxel(map[0] as f32, map[1] as f32, map[2] as f32, perlin);
-        if voxel.0 {
-            break voxel.1;
+
+        if make_coord(map) >= space.len() {
+            break 0;
+        }
+        let voxel = space[make_coord(map)];
+        if voxel != 0 {
+            break voxel; 
         }
     }
 }
 
-fn gen_buffer(width: usize, height: usize, camera_angle: f32, perlin: &Perlin) -> Vec<u32> {
+fn gen_buffer(width: usize, height: usize, camera_angle: f32, space: &Vec<u32>) -> Vec<u32> {
     let mut buffer = vec![0x00FFAA00; width * height];
     for y in 0..height {
         for x in 0..width {
             buffer[y * width + x] = cast_ray((
                     std::f32::consts::PI * (1.0 / 3.0 + (y as f32 / height as f32) / 2.0), 
                     camera_angle + std::f32::consts::PI * (1.0 / 4.0 + (x as f32 / width as f32) / 2.0)
-                    ), 0.0, 0.0, 25.0, perlin);
+                    ), 0.0, 0.0, 25.0, space);
         }
     }
     buffer
 }
 
+fn gen_space() -> Vec<u32> {
+    let perlin = Perlin::new();
+    let mut space: Vec<u32> = vec![0u32; 1_000_000];
+    for x in -50..50i32 {
+        for y in -50..50i32 {
+            let height = (perlin.get([x as f64 / 10.0, y as f64 / 10.0]) *  10.0).floor() as i32; 
+            for z in -50..height {
+                let mut color = 0x00000000;
+                for i in [x, y, z] {
+                    color = color << 8;
+                    color += (i + 50) as u32 * 2;
+                }
+                space[make_coord([x, y, z])] = color;
+            }
+        }
+    }
+    space
+}
+
 fn main() {
+    let space = gen_space();
+
     let mut window = match Window::new(
         "Voxel Render",
         WIDTH / 4,
@@ -72,13 +93,12 @@ fn main() {
         }
     };
 
-    let perlin = Perlin::new();
     let mut camera_angle: f32 = 0.0;
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         let (width, height) = window.get_size();
-        let buffer: Vec<u32> = gen_buffer(width / 4, height / 4, camera_angle, &perlin);
-        camera_angle += 0.05;
+        let buffer: Vec<u32> = gen_buffer(width / 4, height / 4, camera_angle, &space);
+        camera_angle += 0.25;
 
         window
             .update_with_buffer(&buffer, width / 4, height / 4)
